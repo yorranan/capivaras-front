2ddab6ef54cc5dcc3aa6196761672a1c336ce00d
Yorranan <yorranan.costa@outlook.com>
2024-09-30 23:40:11 -0300

    created initial form to notes

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-notes',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './notes.component.html',
-  styleUrl: './notes.component.scss'
+  styleUrl: './notes.component.scss',
 })
 export class NotesComponent {
+  noteForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.noteForm = this.fb.group({
+      capybaraId: new FormControl(0, [Validators.required]),
+      noteObservations: new FormControl('', [Validators.required]),
+      noteComportament: new FormControl('', [Validators.required]),
+    });
+  }
+
+  submit() {
+    if (this.noteForm.valid) {
+      console.log(this.noteForm.value);
+    } else {
+      console.error('Form is invalid');
+    }
+  }
 }
