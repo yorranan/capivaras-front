c42f398ec29f9be82b7c877dfd8214c7c919e867
Yorranan <yorranan.costa@outlook.com>
2024-09-30 23:05:57 -0300

    created habitat form

--- src/app/components/habitats/habitats.component.ts
+++ src/app/components/habitats/habitats.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-habitats',
   standalone: true,
-  imports: [],
   templateUrl: './habitats.component.html',
-  styleUrl: './habitats.component.scss'
+  styleUrls: ['./habitats.component.scss'],
+  imports: [CommonModule, ReactiveFormsModule]
 })
 export class HabitatsComponent {
+  habitatForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.habitatForm = this.fb.group({
+      habitatName: new FormControl('', [Validators.required]),
+      habitatSwitch: new FormControl(false, [Validators.required])
+    });
+  }
+
+  submit() {
+    if (this.habitatForm.valid) {
+      console.log(this.habitatForm.value);
+    } else {
+      console.error('Form is invalid');
+    }
+  }
 
 }
